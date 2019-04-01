a8cf46933fc354be4fd7ed7be1b4581466f49233
alex <rormercury@gmail.com>
2019-04-01 23:49:06 +0300

    Add vue router

--- lib/rails_vue_js/view_helper.rb
+++ lib/rails_vue_js/view_helper.rb
@@ -3,6 +3,7 @@ module RailsVueJs
   module ViewHelper
     def rails_vue_js
       content = javascript_include_tag(vue_js_src)
+      content += javascript_include_tag('vue_router_src')
       content += javascript_include_tag('rails_vue_js')
       Dir[rails_vue_path].each do |path|
         content += render file: path
@@ -18,6 +19,10 @@ module RailsVueJs
       else
         'https://cdn.jsdelivr.net/npm/vue/dist/vue.js'
       end
+    end
+
+    def vue_router_src
+      'https://unpkg.com/vue-router/dist/vue-router.js'
     end
 
     def rails_vue_path
